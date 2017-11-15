1cb48f347025097bd4c726f550d4bdcc49859618
jayeeta1987 <jayeeta1987@outlook.com>
2017-11-15 09:43:39 -0500

    initial commit

---
+++ game.py
@@ -0,0 +1,7 @@
+#this is using to run the game by importing 'drawframe' module.
+import drawframe
+class ticTacToe:
+    def __init__(self):
+        drawframe.mainframe() #calling mainframe() to create first window.
+
+ticTacToe()
