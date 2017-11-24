96b52a02e7b73248320e0d9bb426191e2b0f119e
Jayeeta Banerjee <jayeeta1987@outlook.com>
2017-11-24 11:34:37 -0500

    Delete game.py

--- game.py
+++
@@ -1,7 +0,0 @@
-#this is using to run the game by importing 'drawframe' module.
-import drawframe
-class ticTacToe:
-    def __init__(self):
-        drawframe.mainframe() #calling mainframe() to create first window.
-
-ticTacToe()
